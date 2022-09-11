b023a8dcc81df971f2dbff660c287129e9462ad9
Nil <35883547+NilGamer@users.noreply.github.com>
2022-09-11 07:11:59 +0000

    Basic Weather app

---
+++ yourPlayground.js
@@ -0,0 +1,9 @@
+//Baby weather app
+let weather = prompt("how's the weather today?");
+
+if (weather == 'rainy') {
+  console.log('Grab your umbrella ☔');
+}
+else {
+  console.log('Grab your sunglasses 😎');
+}
