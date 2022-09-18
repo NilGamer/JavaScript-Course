d1c342ef37816365bfe12c95b7f82fff589519f0
Nil <35883547+NilGamer@users.noreply.github.com>
2022-09-18 08:45:08 +0000

    map & filter

--- exercises/arraysorting.js
+++ exercises/arraysorting.js
@@ -4,8 +4,16 @@
 
 // Write a function that takes in an array and sort the numbers inside from least to greatest
 
-function sortArray (array) {
-  
+function sortArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length - 1; j++) {
+      if (array[i] > array[j]) {
+        let tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+      }
+    }
+  }
 }
 
 // BONUS sort the array without using .sort()
--- yourPlayground.js
+++ yourPlayground.js
@@ -53,4 +53,52 @@ const letterFrequency = (phrase) => {
   return frequency;
 }
 
-console.log(letterFrequency("nilesh"));
+// console.log(letterFrequency("nilesh"));
+
+const wordFrequency = (phrase) => {
+  let frequency = {};
+
+  for (const letter of phrase.split(" ")) {
+    if (letter in frequency) {
+      frequency[letter]++;
+    }
+    else {
+      frequency[letter] = 1;
+    }
+  }
+  return frequency;
+}
+
+// console.log(wordFrequency("lol what lol"));
+
+// map
+const doubleMap = (numbers) => {
+  return numbers.map(number => number * 2);
+}
+
+console.log(doubleMap([1, 2, 3, 4]));
+
+// filter
+// const filter = (numbers, greaterThan) => {
+//   let result = [];
+
+//   result = numbers.filter(number => number > greaterThan);
+
+//   return result;
+// }
+
+const nums = [1, 2, 3, 4, 5, 6, 7];
+let result = nums.filter(num => num > 3);
+
+// console.log(result);
+
+const players = [
+  { name: 'dhoni', price: 20, team: 'csk' },
+  { name: 'kohli', price: 19, team: 'rcb' },
+  { name: 'raina', price: 10, team: 'csk' },
+  { name: 'rohit', price: 12, team: 'mi' },
+  { name: 'yuvi', price: 14, team: 'punjab' },
+  { name: 'jadeja', price: 15, team: 'csk' },
+];
+
+console.log(players.filter(player => player.team == 'csk'));
